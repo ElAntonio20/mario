2a0e71c96330d4c4cd9d9aa4253209a4c0e47dd6
ISND 8vo ISND89 9no Semestre <ISND89@AISND20-22.campus.iest.edu.mx>
2023-03-06 18:17:50 -0600

    Initial commit

---
+++ app/src/main/java/iest/edu/actividadfotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package iest.edu.actividadfotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/iest/edu/actividadfotos/Mario.kt
@@ -0,0 +1,11 @@
+package iest.edu.actividadfotos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Mario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mario_main)
+    }
+}
